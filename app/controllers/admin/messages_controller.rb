34fbcd6a87f3fbc8ba1c515330764c7fcee77cef
Sharon Choe <sharonghae@gmail.com>
2016-06-14 08:05:19 -0400

    Added ability to mark messages read and unread

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -7,8 +7,16 @@ class Admin::MessagesController < Admin::ApplicationController
   end
 
   def update
+    @message = Message.find(params[:id])
+    @message.update(status: params[:status])
+
+    redirect_to :back, notice: "Message was successfully updated."
   end
 
   def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+
+    redirect_to :back, notice: "Message was successfully deleted."
   end
 end
