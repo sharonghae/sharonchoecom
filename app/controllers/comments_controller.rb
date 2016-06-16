eca49de2ecac76bfad6cf67c23d589cd48cc7632
Sharon Choe <sharonghae@gmail.com>
2016-06-16 07:59:58 -0400

    Added bootstrap, features, began working on front end

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,28 @@
 class CommentsController < ApplicationController
   def create
+    visitor = Visitor.find_by(email: visitor_comments_params[:email])
 
+    if visitor
+      visitor.tap do |v|
+        v.comments << Comment.new(visitor_comments_params[:comments_attributes['0']])
+      end
+    else
+      visitor = Visitor.new(visitor_comments_params)
+    end
+
+    #save the comments
+    if visitor.save
+      flash[:notice] = "Successfully created a new comment."
+    else
+      flash[:alert] = "There was a problem creating your comment."
+    end
+
+    redirect_to :back
+  end
+
+  private
+
+  def visitor_comments_params
+    params.require(:visitor).permit(:fullname, :email, :comments_attributes => [:message, :post_id])
   end
 end
